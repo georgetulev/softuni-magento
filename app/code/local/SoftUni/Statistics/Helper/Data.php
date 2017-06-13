c4276757f4ccfe9f356b5f4f6b78e70ef7c206ec
2Lev <georgetulev@abv.bg>
2017-06-13 09:28:52 +0300

    Statistics module structure added. Link to our statistic page added to the customer navigation

---
+++ app/code/local/SoftUni/Statistics/Helper/Data.php
@@ -0,0 +1,6 @@
+<?php
+
+class SoftUni_Statistics_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
---
+++ app/code/local/SoftUni/Statistics/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+
+class SoftUni_Statistics_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        $this->loadLayout();
+
+        return $this->renderLayout();
+    }
+}
